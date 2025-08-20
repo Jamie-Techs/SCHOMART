aa04f8e2bd52eb6817b7da31bbf0da71f7f83d48
Jamie-Techs <jamesnwoke090@gmail.com>
2025-08-20 14:43:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'Jamiecoo15012004')
-app.permanent_session_lifetime = timedelta(hours=1)
+
 bcrypt = Bcrypt(app)
 mail = Mail(app)
 oauth = OAuth(app)
@@ -8238,6 +8238,7 @@ def get_advert_info_from_firestore(advert_id):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
