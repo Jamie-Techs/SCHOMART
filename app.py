9c4d5473ca93705f679cd271cbae9dc84514efbd
Jamie-Techs <jamesnwoke090@gmail.com>
2025-08-25 22:55:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3489,7 +3489,10 @@ def upload_message_file():
             
             
             
-            
+   @app.route('/subscribe')
+def subscribe():
+    # Your subscription logic here
+    return render_template('subscribe.html')         
 
 
 # --- Helper Functions to fetch data from DB ---
@@ -6975,6 +6978,7 @@ def send_message():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render gives you the port in $PORT
     app.run(host="0.0.0.0", port=port)
+
 
 
 
