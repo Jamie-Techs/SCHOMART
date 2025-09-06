90ac9ce08d4e44dc262f1228b6b6ffbdf8a19158
Jamie-Techs <jamesnwoke090@gmail.com>
2025-09-06 19:28:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com' # Use your email provider's SMTP se
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'schomartcustomercare@gmail.com'
+app.config['MAIL_USERNAME'] = 'SCHOMART'
 app.config['MAIL_PASSWORD'] = 'gfxv ljqv hgxm qtjg'
 app.config['MAIL_DEFAULT_SENDER'] = 'schomartcustomercare@gmail.com'
 
@@ -5095,6 +5095,7 @@ if __name__ == "__main__":
     scheduler.start()
     
     app.run(host="0.0.0.0", port=port)
+
 
 
 
